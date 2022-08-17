cb6fe63a8c9ced52cf6012ee7d0c1042cacba0e5
Tianling Shen <i@cnsztl.eu.org>
2022-08-17 23:46:37 +0800

    chore: use Loyalsoldier's geosite
    Signed-off-by: Tianling Shen <i@cnsztl.eu.org>

--- main.go
+++ main.go
@@ -54,10 +54,10 @@ func get(downloadURL *string) ([]byte, error) {
 
 func download(release *github.RepositoryRelease) ([]byte, error) {
 	geositeAsset := common.Find(release.Assets, func(it *github.ReleaseAsset) bool {
-		return *it.Name == "dlc.dat"
+		return *it.Name == "geosite.dat"
 	})
 	geositeChecksumAsset := common.Find(release.Assets, func(it *github.ReleaseAsset) bool {
-		return *it.Name == "dlc.dat.sha256sum"
+		return *it.Name == "geosite.dat.sha256sum"
 	})
 	if geositeAsset == nil {
 		return nil, E.New("geosite asset not found in upstream release ", release.Name)
@@ -171,7 +171,7 @@ func release(source string, destination string, output string) error {
 }
 
 func main() {
-	err := release("v2fly/domain-list-community", "sagernet/sing-geosite", "geosite.db")
+	err := release("Loyalsoldier/domain-list-custom", "1715173329/sing-geosite", "geosite.db")
 	if err != nil {
 		logrus.Fatal(err)
 	}
